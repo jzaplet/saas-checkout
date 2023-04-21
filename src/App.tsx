cc74d29314134624ed933392baf0cf5d37344a8a
Jiří Zapletal <jz@strategio.dev>
2023-04-20 23:17:06 -0600

    Refactored App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,20 +6,21 @@ import PersonalInfo from './components/PersonalInfo';
 import SelectPlan from './components/SelectPlan';
 import PickAddons from './components/PickAddons';
 import FinishingUp from './components/FinishingUp';
-import Step from './components/stepper/Step';
 import ThankYou from './components/ThankYou';
+import Step from './components/stepper/Step';
+
+const components = {
+  1: PersonalInfo,
+  2: SelectPlan,
+  3: PickAddons,
+  4: FinishingUp,
+  5: ThankYou,
+};
+
+const stepNames: string[] = ['Your info', 'Select plan', 'Add-ons', 'Summary'];
 
 function App() {
   const [currentStep] = useAtom(step);
-  const steps: string[] = ['Your info', 'Select plan', 'Add-ons', 'Summary'];
-
-  const components = {
-    1: PersonalInfo,
-    2: SelectPlan,
-    3: PickAddons,
-    4: FinishingUp,
-    5: ThankYou,
-  };
 
   return (
     <div className="mx-auto w-[1130px] p-5 flex bg-white shadow-lg rounded-lg">
@@ -27,7 +28,7 @@ function App() {
         <img src={SideBarDesktop} alt="Checkout BG" />
         <div className="absolute inset-0 p-8">
           <div className="flex flex-col gap-y-8">
-            {steps.map((step, i) => (
+            {stepNames.map((step, i) => (
               <Step step={i + 1} name={step} active={i === currentStep - 1 || (currentStep === 5 && i === 3)} key={i} />
             ))}
           </div>
@@ -36,7 +37,7 @@ function App() {
       <div className="flex justify-center w-[855px]">
         <div className="w-[545px]">
           {
-            // @ts-ignore
+            // @ts-ignore // TODO: Fix this
             React.createElement(components[currentStep])
           }
         </div>
