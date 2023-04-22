bddeb32a8c44c2dc81b04360fbacafafbe3dfe77
Jiří Zapletal <jz@strategio.dev>
2023-04-22 10:37:43 -0600

    Added Thank You step

--- src/components/ThankYou.tsx
+++ src/components/ThankYou.tsx
@@ -1,7 +1,18 @@
+import ImgThankYou from '../../assets/images/icon-thank-you.svg';
+
 function ThankYou(): JSX.Element {
   return (
     <form className="grid grid-cols-1 place-content-center text-center h-full">
-      <div>Thank you....</div>
+      <div className="flex flex-col items-center justify-center text-center">
+        <div className="text-center">
+          <img src={ImgThankYou} alt="Thank you" />
+        </div>
+        <h1 className="text-3xl font-bold py-4 text-marine-blue">Thank you!</h1>
+        <p className="text-lg text-cool-gray max-w-[500px]">
+          Thanks for confirming your subscription! We hope you have fun using our platform. If you ever need support,
+          feel free to email us at support@loremgaming.com.
+        </p>
+      </div>
     </form>
   );
 }
